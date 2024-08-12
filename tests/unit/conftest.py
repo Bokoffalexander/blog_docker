599b69c06f0ce8ebecc623bf9f64c9aab29a07c0
Bokoffalexander <89662116+Bokoffalexander@users.noreply.github.com>
2024-08-12 21:34:35 +0300

    Update conftest.py

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,1 +1,12 @@
-hello
+import pytest
+
+from rest_framework.test import APIClient
+
+
+@pytest.fixture(scope="function")
+def api_client() -> APIClient:
+    """
+    Fixture to provide an API client
+    :return: APIClient
+    """
+    yield APIClient()
